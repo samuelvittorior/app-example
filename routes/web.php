60ed09d6864375a4f54bc49d971ebb1f3f835c50
Kali Live user <kali@kali-2024.1-u>
2024-04-27 10:34:45 +0700

    Menambahkan Middleware Token di halaman  about

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\EnsureTokenIsValid;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/about', function () {
     return ('Halaman Tentang Kami');
-});
+})->middleware(EnsureTokenIsValid::class);
+
 Route::get('/contact', function () {
-    return view('Halaman Kontak Kami');
+    return ('Halaman Kontak Kami');
 });
